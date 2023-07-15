073eff134ad320801094c53c1164d261d91a9746
Nathan Doolan <nathan.doolan@gentrack.com>
2023-07-15 15:19:30 +1000

    Remove preprocess-import-assets and bump versions

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,9 @@
-import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/kit/vite";
-import { importAssets } from "svelte-preprocess-import-assets";
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [vitePreprocess(), importAssets()],
+	preprocess: [vitePreprocess()],
 	kit: {
 		adapter: adapter(),
 	},
