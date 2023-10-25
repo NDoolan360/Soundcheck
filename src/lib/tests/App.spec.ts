cc3e333ceb1fd0fe92ab6bf08f7cc1aefd9bf312
NDoolan360 <n.doolan3600@gmail.com>
2023-10-25 21:06:12 +1100

    Add tests for visual settings of art and controls

---
+++ src/lib/tests/App.spec.ts
@@ -0,0 +1,78 @@
+import { render } from '@testing-library/svelte';
+import App from '../../App.svelte';
+import { alwaysShowArtwork, alwaysShowControls, artworkFillMode } from '../settings';
+
+global.fetch = vi.fn().mockImplementation(() =>
+    Promise.resolve({
+        blob: () => Promise.resolve(undefined),
+    })
+) as () => Promise<Response>;
+
+beforeEach(async () => {
+    alwaysShowControls.reset!();
+    alwaysShowArtwork.reset!();
+    artworkFillMode.reset!();
+});
+
+describe('Visual Settings', () => {
+    test('Render default inactive', () => {
+        render(App, { screenActive: false });
+        // Present
+        expect(document.getElementById('track-image')).not.toBeNull();
+        // Absent
+        expect(document.getElementsByTagName('main').length).toBeFalsy();
+    });
+    test('Render default active', () => {
+        render(App, { screenActive: true });
+        // Present
+        expect(document.getElementsByTagName('main').length).toBeTruthy();
+        // Absent
+        expect(document.getElementById('track-image')).toBeNull();
+    });
+    test('Render inactive with alwaysShowArtwork', async () => {
+        await alwaysShowArtwork.set(true);
+        render(App, { screenActive: false });
+        // Present
+        expect(document.getElementById('track-image')).not.toBeNull();
+        // Absent
+        expect(document.getElementsByTagName('main').length).toBeFalsy();
+    });
+    test('Render active with alwaysShowArtwork', async () => {
+        await alwaysShowArtwork.set(true);
+        render(App, { screenActive: true });
+        // Present
+        expect(document.getElementsByTagName('main').length).toBeTruthy();
+        expect(document.getElementById('track-image')).not.toBeNull();
+    });
+    test('Render inactive with alwaysShowControls', async () => {
+        await alwaysShowControls.set(true);
+        render(App, { screenActive: false });
+        // Present
+        expect(document.getElementsByTagName('main').length).toBeTruthy();
+        // Absent
+        expect(document.getElementById('track-image')).toBeNull();
+    });
+    test('Render active with alwaysShowControls', async () => {
+        await alwaysShowControls.set(true);
+        render(App, { screenActive: true });
+        // Present
+        expect(document.getElementsByTagName('main').length).toBeTruthy();
+        // Absent
+        expect(document.getElementById('track-image')).toBeNull();
+    });
+    test('Render with artworkFillMode contain', async () => {
+        await artworkFillMode.set('contain');
+        render(App, { screenActive: false });
+        // Present
+        expect(document.getElementById('track-image')?.style.objectFit === 'contain').toBeTruthy();
+        expect(document.getElementById('lowres-image')).not.toBeNull();
+    });
+    test('Render with artworkFillMode cover', async () => {
+        await artworkFillMode.set('cover');
+        render(App, { screenActive: false });
+        // Present
+        expect(document.getElementById('track-image')?.style.objectFit === 'cover').toBeTruthy();
+        // Absent
+        expect(document.getElementById('lowres-image')).toBeNull();
+    });
+});
